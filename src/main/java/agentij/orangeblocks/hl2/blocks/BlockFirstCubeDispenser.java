7b9eb3829e5c0f96f368ff0d2658a01c63feae65
Ijdtm7 <48657001+Ijdtm7@users.noreply.github.com>
2020-12-21 20:46:40 -0600

    finished cube dispenser. prob last commit for today.

--- src/main/java/agentij/orangeblocks/hl2/blocks/BlockFirstCubeDispenser.java
+++ src/main/java/agentij/orangeblocks/hl2/blocks/BlockFirstCubeDispenser.java
@@ -84,6 +84,36 @@ public class BlockFirstCubeDispenser extends BlockBase {
 
 
     //Why the fuck they all called element.
+    private static final AxisAlignedBB ELEMENT1 = new AxisAlignedBB(-0.188, 0, 0.562, -0.125, 0.25, 0.625);
+    private static final AxisAlignedBB ELEMENT2 = new AxisAlignedBB(-0.25, 0.062, -0.812, 1.188, 0.125, -0.188);
+    private static final AxisAlignedBB ELEMENT3 = new AxisAlignedBB(-0.25, 0.062, 1.188, 1.188, 0.125, 1.812);
+    private static final AxisAlignedBB ELEMENT4 = new AxisAlignedBB(-0.812, 0.062, 0.375, -0.188, 0.125, 0.625);
+    private static final AxisAlignedBB ELEMENT5 = new AxisAlignedBB(1.125, 0.062, 0.375, 1.688, 0.125, 0.625);
+    private static final AxisAlignedBB ELEMENT6 = new AxisAlignedBB(-0.188, 0, 0.375, -0.125, 0.25, 0.438);
+    private static final AxisAlignedBB ELEMENT7 = new AxisAlignedBB(-0.188, 0.188, 0.375, -0.125, 0.25, 0.625);
+    private static final AxisAlignedBB ELEMENT8 = new AxisAlignedBB(-0.188, 0, 0.375, -0.125, 0.062, 0.625);
+    private static final AxisAlignedBB ELEMENT9 = new AxisAlignedBB(-0.188, 0.062, 0.438, -0.125, 0.188, 0.562);
+    private static final AxisAlignedBB ELEMENT10 = new AxisAlignedBB(0.438, -0.688, -0.25, 0.5, 0, -0.188);
+    private static final AxisAlignedBB ELEMENT11 = new AxisAlignedBB(-0.25, -0.688, 0.5, -0.188, 0, 0.562);
+    private static final AxisAlignedBB ELEMENT12 = new AxisAlignedBB(1.125, -0.625, 0.5, 1.188, 0, 0.562);
+    private static final AxisAlignedBB ELEMENT13 = new AxisAlignedBB(0.438, -0.688, 1.188, 0.5, 0, 1.25);
+    private static final AxisAlignedBB ELEMENT14 = new AxisAlignedBB(1.125, 0, 0.5, 1.188, 1.688, 0.562);
+    private static final AxisAlignedBB ELEMENT15 = new AxisAlignedBB(0.438, 0, 1.188, 0.5, 1.688, 1.25);
+    private static final AxisAlignedBB ELEMENT16 = new AxisAlignedBB(-0.25, 0, 0.5, -0.188, 1.688, 0.562);
+    private static final AxisAlignedBB ELEMENT17 = new AxisAlignedBB(0.438, 0, -0.25, 0.5, 1.688, -0.188);
+    private static final AxisAlignedBB ELEMENT18 = new AxisAlignedBB(0.5, 0.125, -0.25, 1.125, 1.688, -0.188);
+    private static final AxisAlignedBB ELEMENT19 = new AxisAlignedBB(-0.188, 0.125, -0.25, 0.438, 1.688, -0.188);
+    private static final AxisAlignedBB ELEMENT20 = new AxisAlignedBB(-0.188, 0.125, 1.188, 0.438, 1.688, 1.25);
+    private static final AxisAlignedBB ELEMENT21 = new AxisAlignedBB(0.5, 0.125, 1.188, 1.125, 1.688, 1.25);
+    private static final AxisAlignedBB ELEMENT22 = new AxisAlignedBB(1.125, 0.125, -0.25, 1.188, 1.688, 0.5);
+    private static final AxisAlignedBB ELEMENT23 = new AxisAlignedBB(1.125, 0.125, 0.562, 1.188, 1.688, 1.25);
+    private static final AxisAlignedBB ELEMENT24 = new AxisAlignedBB(-0.25, 0.125, -0.25, -0.188, 1.688, 0.5);
+    private static final AxisAlignedBB ELEMENT25 = new AxisAlignedBB(-0.25, 0.125, 0.562, -0.188, 1.688, 1.25);
+    private static final AxisAlignedBB ELEMENT26 = new AxisAlignedBB(-0.312, 1.688, -0.312, 1.25, 1.75, -0.188);
+    private static final AxisAlignedBB ELEMENT27 = new AxisAlignedBB(-0.312, 1.688, -0.25, -0.188, 1.75, 1.312);
+    private static final AxisAlignedBB ELEMENT28 = new AxisAlignedBB(1.062, 1.688, -0.25, 1.25, 1.75, 1.312);
+    private static final AxisAlignedBB ELEMENT29 = new AxisAlignedBB(-0.25, 1.688, 1.188, 1.25, 1.75, 1.312);
+
     private static final AxisAlignedBB ELEMENT = new AxisAlignedBB(0.375, 0.062, 0.562, 0.625, 0.125, 0.625);
     private static final AxisAlignedBB ELEMENTa = new AxisAlignedBB(-0.25, 0.062, -0.25, 1.188, 0.125, 0.375);
     private static final AxisAlignedBB ELEMENTb = new AxisAlignedBB(-0.25, 0.062, 0.625, 1.188, 0.125, 1.25);
@@ -138,50 +168,97 @@ public class BlockFirstCubeDispenser extends BlockBase {
     private static final AxisAlignedBB ELEMENTay = new AxisAlignedBB(-0.25, 0.125, 0.562, -0.188, 1.688, 1.25);
     private static final AxisAlignedBB ELEMENTaz = new AxisAlignedBB(-0.312, 1.688, -0.312, 1.25, 1.75, 1.312);
 
+    private static final List<AxisAlignedBB> COLLISION_BOXES_OPEN = Lists.newArrayList(ELEMENT1, ELEMENT2, ELEMENT3, ELEMENT4, ELEMENT5, ELEMENT6, ELEMENT7, ELEMENT8, ELEMENT9, ELEMENT10, ELEMENT11, ELEMENT12, ELEMENT13, ELEMENT14, ELEMENT15, ELEMENT16, ELEMENT17, ELEMENT18, ELEMENT19, ELEMENT20, ELEMENT21, ELEMENT22, ELEMENT23, ELEMENT24, ELEMENT25, ELEMENT26, ELEMENT27, ELEMENT28, ELEMENT29);
+    private static final AxisAlignedBB BOUNDING_BOX_OPEN = new AxisAlignedBB(-0.812, -0.688, -0.812, 1.688, 1.75, 1.812);
+
+
     private static final List<AxisAlignedBB> COLLISION_BOXES = Lists.newArrayList(ELEMENT, ELEMENTa, ELEMENTb, ELEMENTc, ELEMENTd, ELEMENTe, ELEMENTf, ELEMENTg, ELEMENTh, ELEMENTi, ELEMENTj, ELEMENTk, ELEMENTl, ELEMENTm, ELEMENTn, ELEMENTo, ELEMENTp, ELEMENTq, ELEMENTr, ELEMENTs, ELEMENTt, ELEMENTu, ELEMENTv, ELEMENTw, ELEMENTx, ELEMENTy, ELEMENTz, ELEMENTaa, ELEMENTab, ELEMENTac, ELEMENTad, ELEMENTae, ELEMENTaf, ELEMENTag, ELEMENTah, ELEMENTai, ELEMENTaj, ELEMENTak, ELEMENTal, ELEMENTam, ELEMENTan, ELEMENTao, ELEMENTap, ELEMENTaq, ELEMENTar, ELEMENTas, ELEMENTat, ELEMENTau, ELEMENTav, ELEMENTaw, ELEMENTax, ELEMENTay, ELEMENTaz);
     private static final AxisAlignedBB BOUNDING_BOXCLOSED = new AxisAlignedBB(-0.312, 0, -0.312, 1.25, 1.75, 1.312);
     //ugh now i gotta do this shit again for the open model? cmon man.
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
     {
+       if (state.getValue(POWERED))
+       {
+           return BOUNDING_BOX_OPEN;
+       }
         return BOUNDING_BOXCLOSED;
     }
 
     @Override
     public void addCollisionBoxToList(IBlockState state, World world, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entity, boolean isActualState)
     {
-        entityBox = entityBox.offset(-pos.getX(), -pos.getY(), -pos.getZ());
-        for (AxisAlignedBB box : COLLISION_BOXES)
+
+        if (state.getValue(POWERED))
         {
-            if (entityBox.intersects(box))
-                collidingBoxes.add(box.offset(pos));
+            entityBox = entityBox.offset(-pos.getX(), -pos.getY(), -pos.getZ());
+            for (AxisAlignedBB box : COLLISION_BOXES_OPEN)
+            {
+                if (entityBox.intersects(box))
+                    collidingBoxes.add(box.offset(pos));
+            }
+        }else{
+            entityBox = entityBox.offset(-pos.getX(), -pos.getY(), -pos.getZ());
+            for (AxisAlignedBB box : COLLISION_BOXES)
+            {
+                if (entityBox.intersects(box))
+                    collidingBoxes.add(box.offset(pos));
+            }
         }
+
+
     }
 
     @Override
     @Nullable
     public RayTraceResult collisionRayTrace(IBlockState state, World world, BlockPos pos, Vec3d start, Vec3d end)
     {
-        double distanceSq;
-        double distanceSqShortest = Double.POSITIVE_INFINITY;
-        RayTraceResult resultClosest = null;
-        RayTraceResult result;
-        start = start.subtract(pos.getX(), pos.getY(), pos.getZ());
-        end = end.subtract(pos.getX(), pos.getY(), pos.getZ());
-        for (AxisAlignedBB box : COLLISION_BOXES)
+        if (state.getValue(POWERED))
         {
-            result = box.calculateIntercept(start, end);
-            if (result == null)
-                continue;
 
-            distanceSq = result.hitVec.squareDistanceTo(start);
-            if (distanceSq < distanceSqShortest)
+            double distanceSq;
+            double distanceSqShortest = Double.POSITIVE_INFINITY;
+            RayTraceResult resultClosest = null;
+            RayTraceResult result;
+            start = start.subtract(pos.getX(), pos.getY(), pos.getZ());
+            end = end.subtract(pos.getX(), pos.getY(), pos.getZ());
+            for (AxisAlignedBB box : COLLISION_BOXES_OPEN)
             {
-                distanceSqShortest = distanceSq;
-                resultClosest = result;
+                result = box.calculateIntercept(start, end);
+                if (result == null)
+                    continue;
+
+                distanceSq = result.hitVec.squareDistanceTo(start);
+                if (distanceSq < distanceSqShortest)
+                {
+                    distanceSqShortest = distanceSq;
+                    resultClosest = result;
+                }
             }
+            return resultClosest == null ? null : new RayTraceResult(RayTraceResult.Type.BLOCK, resultClosest.hitVec.addVector(pos.getX(), pos.getY(), pos.getZ()), resultClosest.sideHit, pos);
+        }else {
+            double distanceSq;
+            double distanceSqShortest = Double.POSITIVE_INFINITY;
+            RayTraceResult resultClosest = null;
+            RayTraceResult result;
+            start = start.subtract(pos.getX(), pos.getY(), pos.getZ());
+            end = end.subtract(pos.getX(), pos.getY(), pos.getZ());
+            for (AxisAlignedBB box : COLLISION_BOXES)
+            {
+                result = box.calculateIntercept(start, end);
+                if (result == null)
+                    continue;
+
+                distanceSq = result.hitVec.squareDistanceTo(start);
+                if (distanceSq < distanceSqShortest)
+                {
+                    distanceSqShortest = distanceSq;
+                    resultClosest = result;
+                }
+            }
+            return resultClosest == null ? null : new RayTraceResult(RayTraceResult.Type.BLOCK, resultClosest.hitVec.addVector(pos.getX(), pos.getY(), pos.getZ()), resultClosest.sideHit, pos);
         }
-        return resultClosest == null ? null : new RayTraceResult(RayTraceResult.Type.BLOCK, resultClosest.hitVec.addVector(pos.getX(), pos.getY(), pos.getZ()), resultClosest.sideHit, pos);
+
     }
 
 
@@ -192,6 +269,7 @@ public class BlockFirstCubeDispenser extends BlockBase {
             if (worldIn.isBlockPowered(pos) && !diditchange)
             {
                 worldIn.setBlockState(pos, state.withProperty(POWERED, true));
+
                 diditchange=true;
                 int x = pos.getX();
                 int y = pos.getY();
@@ -200,7 +278,7 @@ public class BlockFirstCubeDispenser extends BlockBase {
                 BlockPos cubePos = new BlockPos(x, y - 1, z);
                 IBlockState state1 = worldIn.getBlockState(cubePos);
                 if (state1.getBlock() == Blocks.AIR) {
-                    IBlockState portalState = BlockInit.STORAGECUBE.getDefaultState();
+                    IBlockState portalState = BlockInit.STORAGECUBE.getDefaultState().withProperty(BlockStorageCube.CUBES, 1);
                     worldIn.setBlockState(cubePos, portalState);
                 } else {
                     Main.logger.info("[ERROR] Unable to dispense cube as there is an object obstructing it below");
@@ -208,7 +286,45 @@ public class BlockFirstCubeDispenser extends BlockBase {
                    // for (EntityPlayer player : nearbyplayers) {
                     //    player.sendStatusMessage(new TextComponentString(TextFormatting.GOLD + texttranslated), false);
                   //  }
-                    worldIn.setBlockState(pos, this.getDefaultState());
+                   // worldIn.setBlockState(pos, this.getDefaultState());
+                }
+            }else if (!worldIn.isBlockPowered(pos)){
+                worldIn.setBlockState(pos, state.withProperty(POWERED,false));
+                diditchange=false;
+            }
+            if (state.getValue(POWERED) && !diditchange) {
+
+            }
+        }
+    }
+
+    @Override
+    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+        if (!worldIn.isRemote)  //make sure we are dealing with a server, not client
+        {
+            if (worldIn.isBlockPowered(pos) && !diditchange)
+            {
+                worldIn.setBlockState(pos, state.withProperty(POWERED, true));
+               // worldIn.setBlockState(pos, this.getDefaultState().withProperty(POWERED, true));
+                Main.logger.info("should be giving a change in blockstate");
+
+                diditchange=true;
+                int x = pos.getX();
+                int y = pos.getY();
+                int z = pos.getZ();
+                String texttranslated = new TextComponentTranslation("hl2.message.errorcube").getUnformattedComponentText();
+                BlockPos cubePos = new BlockPos(x, y - 1, z);
+                IBlockState state1 = worldIn.getBlockState(cubePos);
+                if (state1.getBlock() == Blocks.AIR) {
+                    IBlockState portalState = BlockInit.STORAGECUBE.getDefaultState().withProperty(BlockStorageCube.CUBES, 1);
+                   worldIn.setBlockState(cubePos, portalState);
+                } else {
+                    Main.logger.info("[ERROR] Unable to dispense cube as there is an object obstructing it below");
+                    //List<EntityPlayer> nearbyplayers = worldIn.getEntitiesWithinAABB(EntityPlayer.class, new AxisAlignedBB(cubePos).grow(20));
+                    // for (EntityPlayer player : nearbyplayers) {
+                    //    player.sendStatusMessage(new TextComponentString(TextFormatting.GOLD + texttranslated), false);
+                    //  }
+                    //worldIn.setBlockState(pos, this.getDefaultState());
                 }
             }else{
                 worldIn.setBlockState(pos, state.withProperty(POWERED,false));
@@ -219,7 +335,4 @@ public class BlockFirstCubeDispenser extends BlockBase {
             }
         }
     }
-
-
-
 }
